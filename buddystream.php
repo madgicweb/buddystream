06fc35ebd64e849ffa0b776eb860cb55dd485e27
blackphantom <blackphantom@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-30 11:44:24 +0000

    initial start plugin
    git-svn-id: https://plugins.svn.wordpress.org/buddystream/trunk@295153 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ buddystream.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name: BuddyStream
+Plugin URI:http://buddystream.net
+Description: BuddyStream
+Version: 1.0
+Author: Peter Hofman
+Author URI: http://buddystream.net
+*/
+
+// Copyright (c) 2010 BuddyStream.net. All rights reserved.
+//
+// Released under the GPL license
+// http://www.opensource.org/licenses/gpl-license.php
+//
+// This is an add-on for Buddypress
+// http://buddypress.org/
+//
+// **********************************************************************
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// **********************************************************************
+
+/*
+ * Only load code that needs BuddyPress
+ * to run once BP is loaded and initialized.
+ */
+
+//plugin release in a few days
