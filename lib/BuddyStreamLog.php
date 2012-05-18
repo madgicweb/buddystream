2fd82a59e5c2f9a388850606474879b3d4f85dd6
blackphantom <blackphantom@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-18 07:22:54 +0000

    added 2.5.11
    git-svn-id: https://plugins.svn.wordpress.org/buddystream/tags/2.5.11@545644 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/BuddyStreamLog.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Class to provide logging
+ */
+
+class BuddyStreamLog{
+
+   /**
+    * Log a message to the BuddyStream log table
+    * 
+    * @global type $wpdb
+    * @param string $message
+    * @param string $type
+    * @return boolean true
+    */
+    function log($message = "", $type="info"){
+        global $wpdb;
+
+        $results = $wpdb->get_results("SELECT count(id) as count FROM ".$wpdb->prefix."buddystream_log");
+
+        if($results[0]->count >= 1000){
+            $wpdb->query("DELETE FROM ".$wpdb->prefix."buddystream_log ORDER BY id ASC LIMIT 1");            
+        }
+        
+        $wpdb->insert($wpdb->prefix."buddystream_log", array('message' => $message , 'type' => $type));
+        
+        return true;
+    }
+}
