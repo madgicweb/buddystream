59e0c92102173c8713ea07ad444cc2fa4474d3e5
blackphantom <blackphantom@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-11 12:26:59 +0000

    added new version to trunk
    git-svn-id: https://plugins.svn.wordpress.org/buddystream/trunk@570591 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ extensions/flickr/core.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * 
+ * Page loader functions 
+ *
+ */
+
+function buddystream_flickr(){
+    $buddyStreamExtensions = new BuddyStreamExtensions();
+    $buddyStreamExtensions->pageLoader('flickr');
+} 
